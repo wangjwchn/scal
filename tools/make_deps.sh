7edf99a565f5955f8b2f04ac64795091f1d008ff
wangjwchn <wangjwchn@yahoo.com>
2024-10-23 14:25:30 +0200

    update build script

--- tools/make_deps.sh
+++ tools/make_deps.sh
@@ -7,11 +7,11 @@ echo " -> gyp... "
 echo ""
 
 if [[ -d build/gyp ]]; then
-  cd build/gyp
-  git pull
+  echo ""
 else
   mkdir -p build/gyp
   git clone https://chromium.googlesource.com/external/gyp build/gyp
+  cd build/gyp && git checkout caa60026e223fc501e8b337fd5086ece4028b1c6 && cd ../../
 fi
 
 if [ $? -eq 0 ]; then
